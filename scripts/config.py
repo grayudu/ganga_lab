af0bba709565bdbadf0442143d5ebe5600353dc6
Gangadhar Rayudu <grayudu@gmail.com>
2019-05-17 23:11:37 -0400

    added supported files

---
+++ scripts/config.py
@@ -0,0 +1,8 @@
+# config.py
+DATABASE_CONFIG = {
+    'host': 'terraform-20190518021912798900000001.cksyp6usit1p.us-west-2.rds.amazonaws.com',
+    'dbname': 'gangardsmysql',
+    'user': 'admin',
+    'password': 'admin123',
+    'port': 3306
+}
--- scripts/secrets_upload.sh
+++ scripts/secrets_upload.sh
@@ -1,35 +1,24 @@
 #!/bin/sh
-
-#Image upload to s3 bucket
-img_src=""
-repo_s3bucket=""
-
-aws --profile <profile> s3 cp $img_src/sf.jpeg s3://$repo_s3bucket/
-aws --profile <profile> s3api put-object-acl --bucket $repo_s3bucket --key sf.jpeg --acl public-read
-
-# cert and key encryption and upload to secret bucket for nginx configuration
+profile=grayudu
 s3_bucket="gangaapp-secret-101189138796-us-west-2"
 encrypted=/tmp/data.enc
-src="/Users/grayudu/"
+src="./"
 kms_alias='gangaapp'
 key_id=`aws --profile grayudu kms list-aliases --output text --query "Aliases[?AliasName=='alias/$kms_alias'].TargetKeyId | [0]"`
-for i in server.crt server.key
-do
-aws --profile grayudu kms encrypt \
+aws --profile $profile kms encrypt \
     --key-id $key_id \
-    --plaintext fileb://$src/$i \
+    --plaintext fileb://$src/config.py \
     --query CiphertextBlob \
     --output text \
     | base64 --decode \
     > $encrypted
 
-aws --profile grayudu s3api put-object \
+aws --profile $profile s3api put-object \
     --bucket $s3_bucket \
-    --key nginx_$i \
+    --key dbconfig \
     --acl private \
     --body $encrypted \
     --output text \
     --query 'None' \
     | egrep -v '^None$' || true
-done
 
