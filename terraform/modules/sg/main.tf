b5aabe5b719e5569010033f1039a185b71f0c30b
Gangadhar Rayudu <grayudu@gmail.com>
2019-05-18 22:53:44 -0400

    added sg groups separately

--- terraform/modules/sg/main.tf
+++ terraform/modules/sg/main.tf
@@ -2,8 +2,8 @@ data "http" "myip" {
   url = "http://ipv4.icanhazip.com"
 }
 
-resource "aws_security_group" "this" {
-  name        = "${var.name}"
+resource "aws_security_group" "this_bastion" {
+  name        = "${var.name}-ssh"
   description = "${var.desc}"
   vpc_id      = "${var.aws_vpc_id}"
 
@@ -14,6 +14,20 @@ resource "aws_security_group" "this" {
     cidr_blocks = ["${chomp(data.http.myip.body)}/32"]
   }
 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+}
+
+resource "aws_security_group" "this_lb" {
+  name        = "${var.name}-lb"
+  description = "${var.desc}"
+  vpc_id      = "${var.aws_vpc_id}"
+
   ingress {
     # TLS (change to whatever ports you need)
     from_port = 443
@@ -22,7 +36,7 @@ resource "aws_security_group" "this" {
 
     # Please restrict your ingress to only necessary IPs and ports.
     # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
-    cidr_blocks = ["0.0.0.0/0"] # add a CIDR block here
+    cidr_blocks = ["${var.cidr_443}"]
   }
 
   egress {
@@ -31,6 +45,41 @@ resource "aws_security_group" "this" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
 
-    #prefix_list_ids = ["pl-12c4e678"]
+  }
+}
+
+resource "aws_security_group" "this_ec2" {
+  name        = "${var.name}-ec2"
+  description = "${var.desc}"
+  vpc_id      = "${var.aws_vpc_id}"
+
+  ingress {
+    # TLS (change to whatever ports you need)
+    from_port = 443
+    to_port   = 443
+    protocol  = "tcp"
+
+    # Please restrict your ingress to only necessary IPs and ports.
+    # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
+    cidr_blocks = ["${var.cidr_ec2_443}"]
+  }
+
+  ingress {
+    # TLS (change to whatever ports you need)
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+
+    # Please restrict your ingress to only necessary IPs and ports.
+    # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
+    cidr_blocks = ["${var.cidr_ec2_443}"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+
   }
 }
